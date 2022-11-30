66ecef256d28e3fdb2632e0bfdac5c7890ff4609
Alexa Kornél <kornel0120@gmail.com>
2022-11-30 16:38:08 +0100

    Spring boot application is generated and configured.

---
+++ Backend/select-it-delivery/src/main/java/com/example/selectitdelivery/SelectItDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.example.selectitdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelectItDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelectItDeliveryApplication.class, args);
+	}
+
+}
---
+++ Backend/select-it-delivery/src/test/java/com/example/selectitdelivery/SelectItDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.selectitdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelectItDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
